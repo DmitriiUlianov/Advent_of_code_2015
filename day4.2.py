d7c1cc87750c3382b5b7b04c250abe20ef20aa56
Dmitrii Ulianov <107825352+DmitriiUlianov@users.noreply.github.com>
2024-08-19 21:21:51 +0200

    Create day4.2.py

---
+++ day4.2.py
@@ -0,0 +1,17 @@
+import hashlib
+
+key = 'iwrupvqb'
+new_key = key
+digit = 0
+while True:
+        str_digit = str(digit)
+        new_key += str_digit
+        res_object = hashlib.md5(new_key.encode())  # Encode the string to bytes
+        res = res_object.hexdigest()
+        #if res[:5] == '00000':
+        if res[:6] == '000000':
+            print(digit)
+            print(res)
+            break
+        digit += 1
+        new_key = key
