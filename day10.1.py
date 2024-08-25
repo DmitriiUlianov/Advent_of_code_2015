165e2ead0873a608b672b08ab653f447a8326ed5
Dmitrii Ulianov <107825352+DmitriiUlianov@users.noreply.github.com>
2024-08-25 16:05:34 +0200

    Create day10.1.py

---
+++ day10.1.py
@@ -0,0 +1,18 @@
+Str = "3113322113"
+n = 0
+while n < 40:
+    new_Str = ""
+    length = len(Str)
+    idx = 0
+    count = 1
+    while idx < length:
+        if idx < length - 1 and Str[idx] == Str[idx + 1]:
+            count += 1    
+        else:
+            new_Str += str(count)
+            new_Str += Str[idx]
+            count = 1
+        idx += 1 
+    Str = new_Str
+    n += 1
+print(len(Str))
